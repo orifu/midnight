521a9f1432ff58796a92e21ff47df60583d821cb
orifu <orifu@duck.com>
2022-08-19 15:03:09 +0100

    Add settings page

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createCountdown } from './countdown';
 import { getTomorrow } from './util';
 
 const timer = document.getElementById('timer')!;
+const settingsParent = document.getElementById('settings')!;
+
+document.getElementById('openSettings')!.onclick = () =>
+    settingsParent.classList.add('shown');
+document.getElementById('closeSettings')!.onclick = () =>
+    settingsParent.classList.remove('shown');
 
 createCountdown(getTomorrow(), timer, {});
