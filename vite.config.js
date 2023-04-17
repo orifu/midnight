b62c2515d4ae9b0a85b868bb2f9aa18beb759721
orifu <orifu@duck.com>
2023-04-17 16:13:23 +0100

    chore: use hash in built asset names again

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,4 @@
 /** @type {import('vite').UserConfig} */
 export default {
     base: '',
-    build: {
-        rollupOptions: {
-            output: {
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`,
-            },
-        },
-    },
 };
